c849fb7887f823dceb2e8b6f5ab7a0313f7c038a
raichev-dima <dmitry.raichev@epicmax.co>
2020-10-05 15:42:41 +0300

    feat(ceasar-cipher-cli): create the Caesar encoder/decoder

---
+++ caesar-cipher-cli/src/Caesar.js
@@ -0,0 +1,98 @@
+const Errors = {
+  SHIFT_CHECK_ERROR: 'Shift value should be greater or equal to 0',
+};
+
+const ALPHABET = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+function checkUpperCase(ch) {
+  return ch === ch.toUpperCase();
+}
+
+function checkLatinChar(ch) {
+  return typeof ch === 'string' && /^[A-Za-z]$/.test(ch);
+}
+
+function makeShift(ch, shift) {
+  const index = ALPHABET.indexOf(ch);
+  const sign = shift !== 0 ? Math.abs(shift) / shift : 1;
+  const next = (index + shift) % (sign * ALPHABET.length);
+
+  if (index > -1) {
+    return next >= 0 ? ALPHABET[next] : ALPHABET[ALPHABET.length + next];
+  }
+
+  return ch;
+}
+
+function cipher(str, shift) {
+  return str
+    .split('')
+    .map((ch) => {
+      const isLatinChar = checkLatinChar(ch);
+
+      if (!isLatinChar) {
+        return ch;
+      }
+
+      const isUpperCase = checkUpperCase(ch);
+
+      const next = makeShift(ch.toLowerCase(), shift);
+
+      if (isUpperCase) {
+        return next.toUpperCase();
+      }
+
+      return next;
+    })
+    .join('');
+}
+
+function encode(str, shift) {
+  if (shift < 0) {
+    throw new Error(Errors.SHIFT_CHECK_ERROR);
+  }
+
+  return cipher(str, shift);
+}
+
+function decode(str, shift) {
+  if (shift < 0) {
+    throw new Error(Errors.SHIFT_CHECK_ERROR);
+  }
+
+  return cipher(str, -shift);
+}
+
+module.exports = {
+  encode,
+  decode,
+  makeShift,
+  Errors,
+};
---
+++ caesar-cipher-cli/test/Caesar.spec.js
@@ -0,0 +1,48 @@
+const Caesar = require('../src/Caesar');
+
+test('makeShift', () => {
+  expect(Caesar.makeShift('a', 26)).toEqual('a');
+  expect(Caesar.makeShift('a', 3)).toEqual('d');
+  expect(Caesar.makeShift('a', 29)).toEqual('d');
+  expect(Caesar.makeShift('a', 23)).toEqual('x');
+  expect(Caesar.makeShift('e', -1)).toEqual('d');
+  expect(Caesar.makeShift('x', -23)).toEqual('a');
+  expect(Caesar.makeShift('a', -7)).toEqual('t');
+});
+
+test('encode', () => {
+  expect(Caesar.encode('a', 26)).toEqual('a');
+  expect(Caesar.encode('attackatonce', 4)).toEqual('exxegoexsrgi');
+  expect(Caesar.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 23)).toEqual(
+    'XYZABCDEFGHIJKLMNOPQRSTUVW'
+  );
+  expect(
+    Caesar.encode('QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD', 3)
+  ).toEqual('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG');
+  expect(
+    Caesar.encode('КThis is secret. Message about "_" symbol!', 7)
+  ).toEqual('КAopz pz zljyla. Tlzzhnl hivba "_" zftivs!');
+
+  expect(() => Caesar.encode('', -3)).toThrowError(
+    Caesar.Errors.SHIFT_CHECK_ERROR
+  );
+});
+
+test('decode', () => {
+  expect(Caesar.decode('a', 26)).toEqual('a');
+  expect(Caesar.decode('a', 7)).toEqual('t');
+  expect(Caesar.decode('exxegoexsrgi', 4)).toEqual('attackatonce');
+  expect(Caesar.decode('XYZABCDEFGHIJKLMNOPQRSTUVW', 23)).toEqual(
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  );
+  expect(
+    Caesar.decode('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG', 3)
+  ).toEqual('QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD');
+  expect(
+    Caesar.decode('КAopz pz zljyla. Tlzzhnl hivba "_" zftivs!', 7)
+  ).toEqual('КThis is secret. Message about "_" symbol!');
+
+  expect(() => Caesar.decode('', -3)).toThrowError(
+    Caesar.Errors.SHIFT_CHECK_ERROR
+  );
+});
